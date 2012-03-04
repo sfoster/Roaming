3c81ff48dc966313c5fe222c8707fa460d62e921
GameBuilder <aidan@grngrl.com>
2012-03-04 20:32:19 +0000

    Finished rats and goblins and started trolls and snakes

--- src/resources/npc.js
+++ src/resources/npc.js
@@ -31,13 +31,15 @@ npc.goblinShaman = {
     strength: 1,
     hp: 5,
     mp: 10
-  };
+  };// Goblin Shaman has Damage Aura spell/ability
+  //(doubles the damage of accompanying fighters )
 npc.mutantRat = {
     name: 'Mutant Rat',
     strength: 1,
     hp: 5,
     mp: 10
-  };
+  };// Mutant Rat has Poison spell/ability
+  //( does half damage for 2-3 turns after attack )
 npc.goblinWarlord = {
     name: 'Goblin Warlord',
     strength: 6,
@@ -50,6 +52,37 @@ npc.ratHorde = {
     hp: 30,
     mp: 0
   };
+npc.juvinileTroll = {
+    name: 'Juvenile Troll',
+    strength: 8,
+    hp: 50,
+    mp: 0
+  };
+npc.python = {
+    name: 'Python',
+    strength: 10,
+    hp: 30,
+    mp: 0
+  };
+npc.troll = {
+    name: 'Troll',
+    strength: 10,
+    hp: 75,
+    mp: 0
+  };
+npc.anaconda = {
+    name: 'Anaconda',
+    strength: 15,
+    hp: 50,
+    mp: 0
+  };
+npc.trollElder = {
+    name: 'Troll Elder',
+    strength: 4,
+    hp: 25,
+    mp: 20
+  };// Troll Elder has camo spell/ability
+  //(makes him much harder to hit)
 
 
   return npc;
