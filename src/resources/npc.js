57574fc5d5f1433ae3c8355ce64c0ba5cf91de50
Aidan <aidan@grngrl.com>
2012-03-07 21:11:21 +0000

    added range for npcs

--- src/resources/npc.js
+++ src/resources/npc.js
@@ -7,35 +7,35 @@ define([], function(){
     strength: 2,
     hp: 10,
     mp: 0,
-    range: short
+    range: 'short'
   };
-npc.hugeRat = {
+  npc.hugeRat = {
     name: 'Huge Rat',
     strength: 2,
     hp: 10,
     mp: 0,
-    range: short
+    range: 'short'
   };
- npc.goblinChamp = {
+  npc.goblinChamp = {
     name: 'Goblin Champion',
     strength: 4,
     hp: 20,
     mp: 0,
-    range: short
+    range: 'short'
   };
- npc.giantRat = {
+  npc.giantRat = {
     name: 'Giant Rat',
     strength: 4,
     hp: 20,
     mp: 0,
-    range: short
+    range: 'short'
   };
 npc.goblinShaman = {
     name: 'Goblin Shaman',
     strength: 1,
     hp: 5,
     mp: 10,
-    range: short, medium
+    range: 'short, medium'
   };// Goblin Shaman has Damage Aura spell/ability
   //(doubles the damage of accompanying fighters )
 npc.mutantRat = {
@@ -43,7 +43,7 @@ npc.mutantRat = {
     strength: 1,
     hp: 5,
     mp: 10,
-    range: short, medium
+    range: 'short, medium'
   };// Mutant Rat has Poison spell/ability
   //( does half damage for 2-3 turns after attack )
 npc.goblinWarlord = {
@@ -51,14 +51,14 @@ npc.goblinWarlord = {
     strength: 6,
     hp: 30,
     mp: 0,
-    range: short
+    range: 'short'
   };
 npc.ratHorde = {
     name: 'Rat Horde',
     strength: 6,
     hp: 30,
     mp: 0,
-    range:short
+    range:'short'
   };
 
 npc.juvinileTroll = {
@@ -66,35 +66,35 @@ npc.juvinileTroll = {
     strength: 8,
     hp: 50,
     mp: 0,
-    range: short
+    range: 'short'
   };
 npc.python = {
     name: 'Python',
     strength: 10,
     hp: 30,
     mp: 0,
-    range: short
+    range: 'short'
   };
 npc.troll = {
     name: 'troll',
     strength: 14,
     hp: 60,
     mp: 0,
-    range: short
+    range: 'short'
   };
 npc.anaconda = {
     name: 'Anaconda',
     strength: 16,
     hp: 50,
     mp: 0,
-    range: short
+    range: 'short'
   };
 npc.trollElder = {
     name: 'Troll Elder',
     strength: 4,
     hp: 25,
     mp: 20,
-    range: short, medium
+    range: 'short, medium'
   };// Troll Elder has Camo spell/ability
   //(makes him much harder to hit)
 npc.gaintAdder = {
@@ -102,7 +102,7 @@ npc.gaintAdder = {
     strength: 5,
     hp: 15,
     mp: 20,
-    range: short,medium
+    range: 'short,medium'
   };// Gaint Adder has Hypnotism spell/ability 
 // (target loses a turn)
 npc.gaintCobra= {
@@ -110,21 +110,21 @@ npc.gaintCobra= {
     strength: 20,
     hp: 70,
     mp: 0,
-    range: short
+    range: 'short'
   };
   npc.trollHulk = {
     name: 'Troll Hulk',
     strength: 18,
     hp: 80,
     mp: 0,
-    range: short
+    range: 'short'
   };
   npc.dragon = {
     name: 'Dragon',
     strength: 75,
     hp: 500,
-    mp: 40,
-    range: short, medium, long
+    mp: 50,
+    range: 'short, medium, long'
   };// Dragon has Flame Breath ability
   // (ranged attack)
   return npc;
