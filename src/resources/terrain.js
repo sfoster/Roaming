245ff0f3c75f96b1fe95bf052c15eac3d1582828
sfoster <sam@sam-i-am.com>
2012-03-10 03:02:39 +0000

    Turns out we want the terrain types as a module, so they can require and supply their image properties ready-loaded

---
+++ src/resources/terrain.js
@@ -0,0 +1,43 @@
+define([
+  "image!resources/graphics/clear.gif",
+  "image!resources/graphics/dryground.jpg",
+  "image!resources/graphics/desertground.jpg",
+  "image!resources/graphics/marshyground.png",
+  "image!resources/graphics/mountianground.png",
+  "image!resources/graphics/grassground.jpg"
+], function(
+  clearImg,
+  dryGroundImg, 
+  desertImg, 
+  marshImg, 
+  mountainsImg, 
+  plainsImg
+){
+  var tmpContainer = document.createElement("div");
+      tmpContainer.style.cssText = 'position: absolute; top: -2000px; left: -2000px; width: 1000px; height: 1000px';
+  
+  [
+      clearImg,
+      dryGroundImg, 
+      desertImg, 
+      marshImg, 
+      mountainsImg, 
+      plainsImg
+  ].forEach(function(img){
+    if(!(img.width && img.height)){
+      tmpContainer.removeChild(tmpContainer.firstChild);
+      tmpContainer.appendChild(img);
+      console.log("measure image: ", img, img.offsetWidth, img.offsetHeight);
+    } else {
+      console.log("image has dimensions: ", img, img.width, img.height);
+    }
+  });
+  return {
+    "clear":      { img: clearImg },
+    "barren":     { img: desertImg  },
+    "desert":     { img: dryGroundImg }, 
+    "marsh":      { img: marshImg },
+    "mountains":  { img: mountainsImg },
+    "plains":     { img: plainsImg }
+  };
+});
