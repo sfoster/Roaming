b73ae8d20040a6f740798fba155796e3ffeda2a3
Aidan <aidan@grngrl.com>
2012-03-15 19:49:35 +0000

    added 'holy' weapons ( mp bonus )

--- src/resources/weapons.js
+++ src/resources/weapons.js
@@ -29,7 +29,9 @@ define([], function(){
     shortRangeDamage: 1,
     longRangeDamage:0,
     mediumRangeDamage: 10,
-    weaponclass: mediumrange
+    weaponclass: mediumrange,
+    weight: 2,
+    mpbonus: 0
   };
   
   weapons.throwingKnife = {
@@ -117,7 +119,7 @@ define([], function(){
     weight: 7,
     mpbonus: 0
   };
-weapons.morningstar = {
+  weapons.morningstar = {
     name: 'Morningstar',
     shortRangeDamage: 30,
     longRangeDamage: 0,
@@ -172,7 +174,7 @@ weapons.morningstar = {
     weight: 1,
     mpbonus: 0
   };
-weapons.hatchet = {
+  weapons.hatchet = {
     name: 'Hatchet',
     shortRangeDamage: 7,
     longRangeDamage: 0,
@@ -254,6 +256,256 @@ weapons.hatchet = {
     weaponclass: shortrange,
     weight: 3,
     mpbonus: 0
+  };
+   weapons.holyKnife = {
+    name: ' Holy Knife',
+    shortRangeDamage: 5,
+    longRangeDamage: 0,
+    mediumRangeDamage: 0,
+    weaponclass: shortrange,
+    weight: 1,
+    mpbonus: 3
+  };
+  weapons.holyBowandArrow = {
+    name: ' Holy Bow',
+    shortRangeDamage: 3,
+    longRangeDamage: 10,
+    mediumRangeDamage: 15,
+    ammo: 15,
+    weaponclass: longrange,
+    weight: 1,
+    mpbonus: 3
+  };
+  weapons.holyFishingspear = {
+    name: ' Holy Fishing Spear',
+    shortRangeDamage: 1,
+    longRangeDamage:0,
+    mediumRangeDamage: 10,
+    weaponclass: mediumrange,
+    weight: 2,
+    mpbonus: 3
+  };
+ weapons.holyThrowingknife = {
+    name: ' Holy Throwing Knife',
+    shortRangeDamage: 3,
+    longRangeDamage: 0,
+    mediumRangeDamage: 5,
+    ammo: 5,
+    weaponclass: longrange,
+    weight: 2,
+    mpbonus: 3
+  };
+   weapons.holyTrident = {
+    name: 'Holy Trident',
+    shortRangeDamage: 1,
+    longRangeDamage: 0,
+    mediumRangeDamage: 25,
+    weaponclass: mediumrange,
+    weight: 8,
+    mpbonus: 3
+  };
+   weapons.holyJavelin = {
+    name: 'Holy Javelin',
+    shortRangeDamage: 7,
+    longRangeDamage: 15,
+    mediumRangeDamage: 10,
+    ammo: 3,
+    weaponclass: longrange,
+    weight: 3,
+    mpbonus: 3
+  };
+  weapons.holySpear = {
+    name: 'Holy Spear',
+    shortRangeDamage: 1,
+    longRangeDamage: 0,
+    mediumRangeDamage: 15,
+    weaponclass: mediumrange,
+    weight: 4,
+    mpbonus: 3
+  };
+   weapons.holySword = {
+    name: 'Holy Sword',
+    shortRangeDamage: 20,
+    longRangeDamage: 0,
+    mediumRangeDamage: 0,
+    weaponclass: shortrange,
+    weight: 4,
+    mpbonus: 3
+  };
+  weapons.holySling = {
+    name: 'Holy Sling',
+    shortRangeDamage: 5,
+    longRangeDamage: 6,
+    mediumRangeDamage: 7,
+    ammo: 20,
+    weaponclass: longrange,
+    weight: 1,
+    mpbonus: 3
+    };
+     weapons.holyStick = {
+    name: 'Holy Stick',
+    shortRangeDamage: 2,
+    longRangeDamage: 0,
+    mediumRangeDamage: 1,
+    weaponclass: shortrange,
+    weight: 2,
+    mpbonus: 3
+  };
+ weapons.holyClub = {
+    name: 'Holy Club',
+    shortRangeDamage: 6,
+    longRangeDamage: 0,
+    mediumRangeDamage: 1,
+    weaponclass: shortrange,
+    weight: 5,
+    mpbonus: 3
+  };
+ weapons.holyMace = {
+    name: 'Holy Mace',
+    shortRangeDamage: 15,
+    longRangeDamage: 0,
+    mediumRangeDamage: 0,
+    weaponclass: shortrange,
+    weight: 7,
+    mpbonus: 3
+  };
+  weapons.holyMorningstar = {
+    name: 'Holy Morningstar',
+    shortRangeDamage: 30,
+    longRangeDamage: 0,
+    mediumRangeDamage: 0,
+    weaponclass: shortrange,
+    weight: 10,
+    mpbonus: 3
+  };
+  weapons.holyLongbowandArrow = {
+    name: 'Holy Longbow',
+    shortRangeDamage: 4,
+    longRangeDamage: 15,
+    mediumRangeDamage: 20,
+    ammo: 15,
+    weaponclass: longrange,
+    weight: 1,
+    mpbonus: 3
+  };
+   weapons.holyShortsword = {
+    name: 'Holy Shortsword',
+    shortRangeDamage: 15,
+    longRangeDamage: 0,
+    mediumRangeDamage: 0,
+    weaponclass: shortrange,
+    weight: 2,
+    mpbonus: 3
+  };
+  weapons.holyLongsword = {
+    name: 'Holy Longsword',
+    shortRangeDamage: 25,
+    longRangeDamage: 0,
+    mediumRangeDamage: 0,
+    weaponclass: shortrange,
+    weight: 6,
+    mpbonus: 3
+  };
+  weapons.holyBroadsword = {
+    name: 'Holy Broadsword',
+    shortRangeDamage: 30,
+    longRangeDamage: 0,
+    mediumRangeDamage: 0,
+    weaponclass: shortrange,
+    weight: 7,
+    mpbonus: 3
+  };
+  weapons.holyDagger = {
+    name: 'Holy Dagger',
+    shortRangeDamage: 7,
+    longRangeDamage: 0,
+    mediumRangeDamage: 0,
+    weaponclass: shortrange,
+    weight: 1,
+    mpbonus: 3
+  };
+  weapons.holyHatchet = {
+    name: 'Holy Hatchet',
+    shortRangeDamage: 7,
+    longRangeDamage: 0,
+    mediumRangeDamage: 3,
+    weaponclass: shortrange,
+    weight: 4,
+    mpbonus: 3
+  };
+  weapons.holyAxe = {
+    name: 'Holy Axe',
+    shortRangeDamage: 14,
+    longRangeDamage: 0,
+    mediumRangeDamage: 0,
+    weaponclass: shortrange,
+    weight: 5,
+    mpbonus: 3
+  };
+  weapons.holyThrowingaxe = {
+    name: 'Holy Throwing Axe',
+    shortRangeDamage: 4,
+    longRangeDamage: 0,
+    mediumRangeDamage: 6,
+    ammo: 5,
+    weaponclass: longrange,
+    weight: 4,
+    mpbonus: 3
+  };
+  weapons.holyBattleaxe = {
+    name: 'Holy Battleaxe',
+    shortRangeDamage: 25,
+    longRangeDamage: 0,
+    mediumRangeDamage: 0,
+    weaponclass: shortrange,
+    weight: 7,
+    mpbonus: 3
+  };
+ weapons.holyKatana = {
+    name: 'Holy Katana',
+    shortRangeDamage: 20,
+    longRangeDamage: 0,
+    mediumRangeDamage: 0,
+    weaponclass: shortrange,
+    weight: 4,
+    mpbonus: 3
+  };
+  weapons.holyNinja_to = {
+    name: 'Ninja-to',
+    shortRangeDamage: 15,
+    longRangeDamage: 0,
+    mediumRangeDamage: 0,
+    weaponclass: shortrange,
+    weight: 3,
+    mpbonus: 0
+  };
+  weapons.holyThrowingstars = {
+    name: 'Holy Throwing Stars',
+    shortRangeDamage: 5,
+    longRangeDamage: 5,
+    mediumRangeDamage: 10,
+    ammo: 10,
+    weaponclass: longrange,
+    weight: 2,
+    mpbonus: 3
+  };
+   weapons.holyStaff = {
+    name: 'Holy Staff',
+    shortRangeDamage: 6,
+    longRangeDamage: 0,
+    mediumRangeDamage: 0,
+    weaponclass: shortrange,
+    weight: 3,
+    mpbonus: 8
+  };
+   weapons.holySabre = {
+    name: 'Holy Sabre',
+    shortRangeDamage: 15,
+    longRangeDamage: 0,
+    mediumRangeDamage: 0,
+    weaponclass: shortrange,
+    weight: 3,
+    mpbonus: 3
   };
   return weapons;
 });
