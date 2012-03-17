8b80e5dcc7da1781c96b64ce648760f91b2a35ee
sfoster <sam@sam-i-am.com>
2012-03-17 14:29:12 +0000

    Don't include the current tile in getEdges

--- src/resources/world.js
+++ src/resources/world.js
@@ -12,6 +12,7 @@ define(['$', 'json!data/location/world.json'], function($, worldData){
     exit: exit,
     getEdges: function(x,y){
       var nearby = worldData.filter(function(tile){
+        if(tile.x==x && tile.y==y) return false;
         if(
           Math.abs(tile.x - x) <= 1
         ){
