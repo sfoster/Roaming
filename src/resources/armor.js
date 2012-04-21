7a6c39737ea9a138c26e7ff26a5258befec10c47
Aidan <aidan@grngrl.com>
2012-04-21 11:13:13 +0100

    added some armor

---
+++ src/resources/armor.js
@@ -0,0 +1,33 @@
+define([], function(){
+  
+  var armor = {};
+  armor.rustyBreastplate = {
+  effect: 5/100,
+  encumberance: 2
+  };
+  armor.rustyGauntlets = {
+  effect: 1/100,
+  encumberance: 1
+  };
+  armor.rustySabatons = {
+  effect: 1/100,
+  encumberance: 3
+  };
+  armor.rustyGreaves = {
+  effect: 3/100,
+  encumberance:1
+  };
+  armor.rustyBracers = {
+  effect: 2/100,
+  encuberance: 1
+  };
+  armor.rustyHelmet = {
+  effect: 5/100,
+  encumberance: 1 
+  };
+  armor.rustyshield = {
+  effect: 6/100,
+  encumberance: 2
+  };
+  return armor;
+});
