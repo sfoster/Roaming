83c6b4591700836f83d02e2e8a7a98f1da77e102
Aidan <aidan@grngrl.com>
2012-04-21 10:52:34 +0100

    added on some misc items

---
+++ src/resources/items.js
@@ -0,0 +1,31 @@
+define([], function(){
+  
+  var items = {};
+ 
+ items.edibleMushroom = {
+    name: 'Mushrooms',
+    type: 'food'
+  };
+items.apple = {
+    name: 'Apples',
+    type: 'food'
+  };
+items.pear = {
+    name: 'Pears',
+    type: 'food'
+  };
+items.rope = {
+    name: 'Rope',
+    type: 'special'
+  };
+items.lantern = {
+    name: 'Lantern',
+    type: 'special'
+  };
+items.whetstone = {
+    name: 'Whetstone',
+    type: 'enhancement'
+  };  
+  return items;
+});
+
