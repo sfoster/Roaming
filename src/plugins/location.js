6a97f508aa89ccc98bae756f438780858fbf00e7
sfoster <sam@sam-i-am.com>
2012-05-05 10:39:37 +0100

    Refactor to use a get method to load the location data

--- src/plugins/location.js
+++ src/plugins/location.js
@@ -1,14 +1,25 @@
-define(['$', 'compose', 'models/Location'], function($, Compose, Location){
+define(['$', 'compose', 'lib/json/ref', 'models/Location'], function($, Compose, json, Location){
   console.log("loading location plugin");
   
   window.locations = {};
+
+  function get(url, callback){
+    console.log("get url: ", url);
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      success: callback,
+      error: function(err){
+        console.error("Error loading " + url, err);
+      }
+    });
+  }
 
   // usage: require(['plugins/location!0,0'], function(tile, region){ ... })
   var global = window, 
       config = global.config || (global.config = {});
   
   var locationPlugin = {
-    // pluginBuilder: './locationBuilder',
     
     load: function (coords, req, onLoad, requireConfig) {
       console.log("location plugin load: ", coords);
@@ -16,22 +27,14 @@ define(['$', 'compose', 'models/Location'], function($, Compose, Location){
       if(locn) {
         onLoad(locn);
       } else {
-        $.ajax({
-          dataType: 'json',
-          url: '/location/' + coords + '.json',
-          success: function(resp){
-            var ctorModule = resp.terrainType || 'models/Location';
-            require([ctorModule], function(Clazz){
-              var tile = new Clazz(resp); 
-              onLoad(tile);
-            });
-          }, 
-          error: function(err) {
-            onLoad(err);
-          }
+        get('/location/' + coords + '.json', function(resp){
+          var ctorModule = resp.terrainType || 'models/Location';
+          require([ctorModule], function(Clazz){
+            var tile = new Clazz(resp); 
+            onLoad(tile);
+          });
         });
       }
-      
     }
   };
 
