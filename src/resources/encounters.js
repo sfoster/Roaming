fedd727fd7b526ee0f29d65a8ef838c1895afdde
sfoster <sam@sam-i-am.com>
2012-05-06 17:37:12 +0100

    We could do encounters using this same module format maybe?

---
+++ src/resources/encounters.js
@@ -0,0 +1,11 @@
+define([], function(){
+  
+  var encounters = {};
+  
+  encounters.beginning = {
+    firstVisit: ['Your adventure starts here'],
+    reVisit: ['Your adventure started here. You have a long way to go still']
+  };
+
+  return encounters;
+});
