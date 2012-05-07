b1fb48cde14b9a07ca2b9932ae1bf148aef0a664
sfoster <sam@sam-i-am.com>
2012-05-07 20:08:21 +0100

    Simple initial/revisit encounter text. New (placeholder) style for encounter text

--- src/resources/encounters.js
+++ src/resources/encounters.js
@@ -3,7 +3,10 @@ define([], function(){
   var encounters = {};
   
   encounters.beginning = {
-    firstVisit: ['Your adventure starts here'],
+    firstVisit: [
+      'Your adventure starts here',
+      'You dont quite know what you need to do, but you seem to be on the right track'
+    ],
     reVisit: ['Your adventure started here. You have a long way to go still']
   };
 
