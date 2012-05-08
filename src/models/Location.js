47fcd21b2a146f90889462dcf0f49ed528d1913c
sfoster <sam@sam-i-am.com>
2012-05-08 01:16:01 +0100

    Refactor and make Locations event emitters

--- src/models/Location.js
+++ src/models/Location.js
@@ -15,41 +15,42 @@ define(['$', 'lib/util', 'lib/event'], function($, util, Evented){
       this.id = coords.join(',');
     }
   }
-  Location.prototype.get = function(name){
-    return this[name];
-  };
-  Location.prototype.enter = function(player, game){
-    var proceed = true;
-    emit("onbeforelocationenter", {
-      target: this,
-      player: player,
-      cancel: function(){ proceed = false; }
-    });
-    if(proceed){
-      console.log("location enter: ", this, player, game, player.history);
-      // what is in this tile? 
-      // does anything happen as I enter?
-      //  run any encounters
-      
-      // update the player's history with details of this visit
-      // have I been here before?
-      //  check player.history for this location id
-      var locationHistory = player.history[this.id] || (player.history[this.id] = {}), 
-          visits = locationHistory.visits || (locationHistory.visits = []);
-
-      visits.push(+new Date);
-
-      emit("onafterlocationenter", {
+  util.mixin(Location.prototype, Evented, {
+    get: function(name){
+      return this[name];
+    }, 
+    enter: function(player, game){
+      var proceed = true;
+      emit("onbeforelocationenter", {
         target: this,
         player: player,
         cancel: function(){ proceed = false; }
       });
+      if(proceed){
+        console.log("location enter: ", this, player, game, player.history);
+        // what is in this tile? 
+        // does anything happen as I enter?
+        //  run any encounters
+      
+        // update the player's history with details of this visit
+        // have I been here before?
+        //  check player.history for this location id
+        var locationHistory = player.history[this.id] || (player.history[this.id] = {}), 
+            visits = locationHistory.visits || (locationHistory.visits = []);
+
+        visits.push(+new Date());
+
+        emit("onafterlocationenter", {
+          target: this,
+          player: player,
+          cancel: function(){ proceed = false; }
+        });
+      }
+    }, 
+    exit: function(player, game){
+      console.log("Location exit stub");
     }
-    
-  };
-  Location.prototype.exit = function(player, game){
-    console.log("Location exit stub");
-  };
+  });
 
   return Location;
 });
