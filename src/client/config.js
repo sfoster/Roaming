67f7eb5afcc758763e6f94d7a590324b8aca8edb
sfoster <sam@sam-i-am.com>
2012-09-19 11:51:30 +0100

    config configures the loader, and pulls any environment/context overrides from querystring

---
+++ src/client/config.js
@@ -0,0 +1,41 @@
+// loader config
+require.config({
+  baseUrl: "/",
+  paths: {
+    // path mappings
+    dollar: 'vendor/zepto',
+    text: 'plugins/vendor/text',
+    json: 'plugins/vendor/json',
+    image: 'plugins/vendor/image',
+    order: 'plugins/vendor/order',
+    promise: 'vendor/store/lib/Promise',
+    util: 'lib/util',
+    lang: 'vendor/lodash',
+    compose: 'vendor/compose',
+    knockout: 'vendor/knockout'
+  },
+  packages: [
+    { name: 'store',   location: './vendor/store',    main: 'main' }
+  ],
+  waitSeconds: 15
+});
+
+// environment config
+var config = {};
+
+(function(){
+  // pull config from querystring
+  var expectedKeys = { playerid: true };
+  
+  var queryStr = location.search.substring(1);
+  var pairs, nameValue, params = {};
+  if(queryStr){
+    pairs = queryStr.split('&');
+    for(var i=0; i<pairs.length; i++) {
+      nameValue = pairs[i].split('=');
+      if(nameValue[0] && (nameValue[0] in expectedKeys)){
+        config[ nameValue[0] ] = nameValue[1];
+      }
+    }
+  }
+})();
