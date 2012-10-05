56860437c006e2f037d434c723515b8da01a0720
Sam Foster <sam@sam-i-am.com>
2012-10-05 19:55:32 +0100

    Object store implementation

---
+++ src/client/vendor/store/main.js
@@ -0,0 +1,12 @@
+define([
+  './Cache', './LocalStorage', './Memory', './Observable', './JsonRest'
+], function(Cache, LocalStorage, Memory, Observable, JsonRest){
+  return {
+    Cache: Cache,
+    LocalStorage: LocalStorage,
+    Memory: Memory, 
+    Memory: Memory, 
+    Observable: Observable,
+    JsonRest: JsonRest
+  };
+});
