1d6cbbd0df8df2a0a039ecd2d194f36162588332
Sam Foster <sfoster@sneetchworks.com>
2013-04-05 00:07:34 +0100

    Stubbing out combat with basic round and start/strike/end events

--- src/client/models/Combat.js
+++ src/client/models/Combat.js
@@ -1,74 +1,116 @@
 define([
-	'lib/util',
-	'promise'
+  'lib/util',
+  'promise'
 ], function(util, Promise){
 
-	function Combat(args){
-		util.mixin(this, args || {});
-	}
+  function sumResults(result1, result2) {
+    return {
+      allies: {
+        inflicted: result1.allies.inflicted+result2.allies.inflicted,
+        sustained: result1.allies.sustained+result2.allies.sustained
+      },
+      opponents: {
+        inflicted: result1.opponents.inflicted+result2.opponents.inflicted,
+        sustained: result1.opponents.sustained+result2.opponents.sustained
+      }
+    };
+  }
+  function not(func) {
+    return function(thing){
+      return !func(thing);
+    }
+  }
+  function alive(thing) {
+    return thing.stats.health >= 0;
+  }
 
-	Combat.prototype.roundInterval = 500;
+  function Combat(args){
+    util.mixin(this, args || {});
+  }
 
-	Combat.prototype.start = function(side1, side2) {
+  Combat.prototype.roundInterval = 500;
+
+  Combat.prototype.start = function(allies, opponents) {
     var defd = Promise.defer();
-		var self = this;
-		var finalResult = { killed: [] };
+    var self = this;
+    var finalResult = {
+      allies: { inflicted: 0, sustained: 0 },
+      opponents: { inflicted: 0, sustained: 0 }
+    };
 
     var itv = setInterval(function(){
-      console.log("Combat! ", side2.length + " monsters");
-      var result = self.round(side1, side2);
-      finalResult.killed = finalResult.killed.concat( result.killed );
-      side2 = result.remaining;
+      game.emit("combatroundstart", {
+        scoreboard: finalResult,
+        target: this,
+        allies: allies,
+        opponents: opponents
+      });
+      var result = self.round(allies, opponents);
+      finalResult = sumResults(finalResult, result);
 
-      if(side1.length && side2.length) {
-	        defd.progress(result);
+      game.emit("combatroundend", {
+        target: this,
+        scoreboard: finalResult,
+        result: result
+      });
+
+      if(allies.filter(alive).length && opponents.filter(alive).length) {
+          defd.progress(finalResult);
       } else {
-        clearInterval(itv); 
+        clearInterval(itv);
         defd.resolve(finalResult)
         return;
       };
     }, this.roundInterval);
 
     return defd.promise;
-	}
+  }
 
-	Combat.prototype.round = function(side1, side2) {
-		var combatants = [].concat(side1,side2);
-		console.log("Round combatants: ", combatants);
-
-		// faux battle
-		side2.forEach(function(thing){
-			thing.stats.health--;
-		});
-		// end faux battle
-
-    var deadMonsters = side2.filter(function(monster){
-      return monster.stats.health <= 0;
-    });
-    console.log("You killed: ", deadMonsters.map(function(monster){ return monster.name; }));
-
-    side1 = side1.filter(function(thing){
-      return thing.stats.health > 0;
-    });
-    side2 = side2.filter(function(thing){
-      return thing.stats.health > 0;
-    });
-    return {
-      killed: deadMonsters,
-      remaining: side2
+  Combat.prototype.round = function(allies, opponents) {
+    var combatants = [].concat(allies,opponents);
+    var result = {
+      allies: { inflicted: 0, sustained: 0 },
+      opponents: { inflicted: 0, sustained: 0 }
     };
-	}
 
+    // faux battle
+    opponents.filter(alive).forEach(function(thing){
+      var damage = 1; // should be calculated somehow
+      var ally = allies[0]; // could be random or turn-based or whatever
+
+      // update our tallies
+      thing.stats.health-=damage;
+      result.allies.inflicted+=damage;
+      result.opponents.sustained+=damage;
+
+      game.emit("combatstrike", {
+        target: this,
+        attacker: allies[0],
+        defender: thing,
+        damage: damage
+      });
+    });
+    // end faux battle
+
+    // flag all our dead combatants
+    [].concat(opponents, allies).filter(not(alive)).forEach(function(thing){
+      thing.dead = true;
+    });
+
+    return result;
+  }
+
+  // not hooked up
   function combat(player, monsters) {
     var damage = player.damage(1); // short-range attack
     var damageInflicted = 0;
 
-    // player attach - he magically attacks them all for now 
+    // player attach - he magically attacks them all for now
     monsters.forEach(function(monster){
       var chanceToHit = 0.5;
-      var doesHit = Math.random() >= chanceToHit; 
+      var doesHit = Math.random() >= chanceToHit;
       if(doesHit) {
-        
+
         console.log("You hit for " + damage + " of " + monster.stats.health);
         monster.stats.health = monster.stats.health - damage;
         damageInflicted += damage;
