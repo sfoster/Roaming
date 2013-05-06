0114bd5d2d67adadb89457279325e420244543b2
Sam Foster <sfoster@sneetchworks.com>
2013-05-06 17:39:27 +0100

    Extend knockout's observableArray with a transferItem method to move array contents from one array to another

--- src/client/lib/koHelpers.js
+++ src/client/lib/koHelpers.js
@@ -1,6 +1,30 @@
 // koHelpers.js
 define(['knockout', 'lib/util'], function(ko, util){
-  function makeObservable(obj, depth) {
+
+  function updateArray(arr, newItems) {
+    if(newItems) {
+      arr.splice.apply(arr, [0, arr.length].concat(newItems));
+    } else {
+      arr.splice(0, arr.length);
+    }
+  }
+
+  ko.observableArray.fn.transferItem = function(item, collection, toIndex) {
+    var fromIdx = this.indexOf(item);
+    if('string' == typeof collection) {
+      collection = util.getObject(collection);
+    }
+    if(fromIdx < 0) {
+      console.warn("koHelpers: moveItemTo: item not in this array");
+      return;
+    }
+    this.splice(fromIdx, 1);
+    toIndex = (undefined == toIndex || toIndex < 0) ? collection.length : toIndex;
+    collection.splice(toIndex, 0, item);
+  };
+
+  function makeObservable(obj, options) {
+    options = options || {};
     if(ko.isObservable( obj )){
       return obj;
     }
@@ -10,15 +34,15 @@ define(['knockout', 'lib/util'], function(ko, util){
     var observable = {};
     depth = depth || 0;
     if('array' == type) {
-      if(depth >= 0) {
-        value = value.map(function(item, i){
-          return makeObservable(item, depth-1);
-        });
-      }
+      updateArray(value, value.map(function(item, i){
+        return makeObservable(item, depth-1);
+      }));
+      // array() -> array of observable items
+      // array()[0]() -> the original item. Bleagh
       return ko.observableArray(value);
     } else if(/^object/.test(type)) {
       // ko provides no equivalent to observableArray for objects - to watch property changes
-      // so, optinally walk the tree but return the object itself as-is
+      // so, optionally walk the tree but return the object itself as-is
       if(depth >= 0) {
         Object.keys(value).forEach(function(key){
           value[key] = makeObservable(value[key], depth-1);
@@ -35,8 +59,9 @@ define(['knockout', 'lib/util'], function(ko, util){
   }
 
   return {
-  	makeObservable: makeObservable, 
-  	resolveObservable: resolveObservable
+  	makeObservable: makeObservable,
+  	resolveObservable: resolveObservable,
+    updateArray: updateArray
   };
 })
 
